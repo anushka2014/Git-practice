90ec5ea9619abf7810bfd4f0e873e758fb122e90
Anushka <anuroy1403@gmail.com>
2025-07-19 14:24:03 +0530

    my first commit

---
+++ practicegit.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+public  class practicegit {
+	
+	
+	public static int Findmax(int a[]) {
+		int max = Integer.MIN_VALUE;
+		
+		for(int i =0;i<a.length;i++)
+		{
+			if (max < a[i])
+			max= a[i];
+		}
+		
+		return max;
+	}
+	public static int[] Arrayreturnn()
+	{
+//		int a[] = {1,2,3,4,5};
+//		return a;
+		//or
+		return new int[] {1,2,3,4,5};
+	}
+	
+
+	public static void main(String[] args) {
+	
+		int b[]= {10,20,20,39,10926982,226567,99,2};
+		
+		practicegit maximum = new practicegit();
+		int highst = maximum.Findmax(b);
+//		System.out.println(highst);
+		
+		
+		System.out.println(Arrays.toString(maximum.Arrayreturnn()));
+		
+		
+
+	}
+
+}
